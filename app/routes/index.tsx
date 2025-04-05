283568debd589a00fc0acb0d287d702a300e0623
wn <csignore@whril.com>
2025-04-04 23:18:25 -0400

    11

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -23,8 +23,8 @@ export default function Index() {
       </div>
       <div className="inner-content">
       <div className="text">
-         <h1>For the Doers.</h1>
-         <p>The leading newsletter results driven tech execs and founders read daily.</p>
+         <h1>To the Top.</h1>
+         <p>The leading newsletter high networth people read daily.</p>
       </div>
       <form method="post" action="https://app.jeffamzn.com/subscription/form">
           <p><input className="email" type="email" name="email" required placeholder="Email" /></p>
