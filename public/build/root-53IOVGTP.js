a445a1185a86961d164bbf0a9b7485ece48fe98f
wn <45404661+ob-xyz@users.noreply.github.com>
2025-10-04 16:27:08 -0400

    123

--- public/build/root-53IOVGTP.js
+++ public/build/root-53IOVGTP.js
@@ -234,7 +234,7 @@ function Footer() {
 }
 
 // app/style/global/global.css
-var global_default = "/build/_assets/global-NDQKIDUG.css";
+var global_default = "/build/_assets/global-JMFVPI5Q.css";
 
 // app/root.tsx
 var import_jsx_dev_runtime2 = __toESM(require_jsx_dev_runtime());
@@ -317,4 +317,4 @@ export {
   links,
   meta
 };
-//# sourceMappingURL=/build/root-ZLZRYOLV.js.map
+//# sourceMappingURL=/build/root-53IOVGTP.js.map
