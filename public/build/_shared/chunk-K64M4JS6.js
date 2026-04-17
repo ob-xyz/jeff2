26d108ec161bc49ec067f5af8acabf162e49b750
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-17 19:11:26 -0400

    112

---
+++ public/build/_shared/chunk-K64M4JS6.js
@@ -0,0 +1,7 @@
+// public/img/ja2.png
+var ja2_default = "/build/_assets/ja2-KMGCXLWX.png";
+
+export {
+  ja2_default
+};
+//# sourceMappingURL=/build/_shared/chunk-K64M4JS6.js.map
---
+++ public/build/routes/ads/ad-form-WAX326PY.js
@@ -0,0 +1,154 @@
+import {
+  imageads_default
+} from "/build/_shared/chunk-27F4L5UE.js";
+import {
+  ja2_default
+} from "/build/_shared/chunk-K64M4JS6.js";
+import {
+  AltchaWrapper
+} from "/build/_shared/chunk-552RS725.js";
+import {
+  Link
+} from "/build/_shared/chunk-4RXSKUQF.js";
+import {
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-S4ZNHW4H.js";
+import {
+  __toESM
+} from "/build/_shared/chunk-IU43IUTG.js";
+
+// app/routes/ads/ad-form.tsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "adform-container", children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "logo", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { src: ja2_default, alt: "The Poast Logo" }, void 0, false, {
+      fileName: "app/routes/ads/ad-form.tsx",
+      lineNumber: 12,
+      columnNumber: 9
+    }, this) }, void 0, false, {
+      fileName: "app/routes/ads/ad-form.tsx",
+      lineNumber: 11,
+      columnNumber: 7
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "content", children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "inner-content", children: [
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "text", children: [
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h4", { children: "ADVERTISE WITH US" }, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 17,
+            columnNumber: 13
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h1", { children: "Reach 12k+" }, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 18,
+            columnNumber: 13
+          }, this)
+        ] }, void 0, true, {
+          fileName: "app/routes/ads/ad-form.tsx",
+          lineNumber: 16,
+          columnNumber: 11
+        }, this),
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("form", { method: "post", action: "https://app.jeffamzn.com/subscription/form", children: [
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { className: "email mb", type: "text", name: "firstName", required: true, placeholder: "First Name *" }, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 21,
+            columnNumber: 25
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { className: "email mb", type: "text", name: "lastName", required: true, placeholder: "Last Name *" }, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 22,
+            columnNumber: 25
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { className: "email mb", type: "text", name: "company", required: true, placeholder: "Company *" }, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 23,
+            columnNumber: 25
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "input-wrapper", children: [
+            /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { className: "email", type: "email", name: "email", required: true, placeholder: "Business Email Address *" }, void 0, false, {
+              fileName: "app/routes/ads/ad-form.tsx",
+              lineNumber: 25,
+              columnNumber: 27
+            }, this),
+            /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("button", { className: "submit", type: "submit", children: "Let's do it" }, void 0, false, {
+              fileName: "app/routes/ads/ad-form.tsx",
+              lineNumber: 26,
+              columnNumber: 27
+            }, this)
+          ] }, void 0, true, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 24,
+            columnNumber: 25
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(AltchaWrapper, {}, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 28,
+            columnNumber: 25
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { id: "bcd05", type: "hidden", name: "l", value: "bcd05274-b4b9-4a90-a8b5-1fdee92637fa" }, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 29,
+            columnNumber: 25
+          }, this),
+          /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("input", { type: "hidden", name: "nonce" }, void 0, false, {
+            fileName: "app/routes/ads/ad-form.tsx",
+            lineNumber: 30,
+            columnNumber: 25
+          }, this)
+        ] }, void 0, true, {
+          fileName: "app/routes/ads/ad-form.tsx",
+          lineNumber: 20,
+          columnNumber: 23
+        }, this)
+      ] }, void 0, true, {
+        fileName: "app/routes/ads/ad-form.tsx",
+        lineNumber: 15,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "inner-content1", children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("img", { src: imageads_default, alt: "The Poast newsletter image" }, void 0, false, {
+        fileName: "app/routes/ads/ad-form.tsx",
+        lineNumber: 34,
+        columnNumber: 11
+      }, this) }, void 0, false, {
+        fileName: "app/routes/ads/ad-form.tsx",
+        lineNumber: 33,
+        columnNumber: 9
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/routes/ads/ad-form.tsx",
+      lineNumber: 14,
+      columnNumber: 7
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { className: "inner-content2", children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h2", { children: [
+        "Connect with your next customers on ",
+        /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("span", { children: "The Poast" }, void 0, false, {
+          fileName: "app/routes/ads/ad-form.tsx",
+          lineNumber: 38,
+          columnNumber: 49
+        }, this)
+      ] }, void 0, true, {
+        fileName: "app/routes/ads/ad-form.tsx",
+        lineNumber: 38,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(Link, { className: "text", to: "/ads/ad-form", children: "Our audience is bold, decisive, and always looking for something new. So whether you want to use videos, images, or words - there's a The Poast ad format that will help you reach your goals." }, void 0, false, {
+        fileName: "app/routes/ads/ad-form.tsx",
+        lineNumber: 39,
+        columnNumber: 11
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/routes/ads/ad-form.tsx",
+      lineNumber: 37,
+      columnNumber: 7
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/routes/ads/ad-form.tsx",
+    lineNumber: 10,
+    columnNumber: 5
+  }, this);
+}
+export {
+  Index as default
+};
+//# sourceMappingURL=/build/routes/ads/ad-form-WAX326PY.js.map
