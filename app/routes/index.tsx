ccf330458cf2376778416c755bc9a842d0645214
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-19 02:49:26 -0400

    11

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
       <div className="header">
         <img className="logo" src={tp} alt="The Poast Logo" />
         <p>Canadian business news in 1-minute</p>
-        <p>Join 12k+ readers every day</p>
+        <p>Join 12k+ daily readers</p>
         <div className="outer-header">
           <div className="inner-header">
             <div className="social">
