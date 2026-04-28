daf15c1287fc4f90d1ce0029003ae0090c9c0a27
wn <45404661+ob-xyz@users.noreply.github.com>
2026-04-28 14:20:47 -0400

    1

---
+++ app/routes/ads/advertise-with-us.tsx
@@ -0,0 +1,54 @@
+import { Link } from "@remix-run/react";
+import Altcha from "../components/altcha";
+
+import li from "~/../public/img/social/linkedin.png";
+import x from "~/../public/img/social/x.png";
+import ig from "~/../public/img/social/instagram.png";
+import info from "~/../public/img/social/info.png";
+import j from "~/../public/img/ja7.png";
+import logo from "~/../public/img/ja.png";
+export default function Index() {
+  return (
+    <div className="subscribe-container">
+      <div className="header">
+        <img className="logo" src={logo} alt="The Poast Logo" />
+        <p>Tech news in 1-minute</p>
+        <p>Subscribe for free</p>
+        <div className="outer-header">
+          <div className="inner-header">
+            <div className="social">
+                <a className="x" href="https://x.com/thepoast" target="_blank" rel="noopener noreferrer">
+                <img src={x} alt="X (Twitter)" />
+              </a>
+                <a className="ig" href="https://instagram.com/thepoast" target="_blank" rel="noopener noreferrer">
+                <img src={ig} alt="Instagram" />
+              </a>
+              <a className="li" href="https://linkedin.com/company/thepoast" target="_blank" rel="noopener noreferrer">
+                <img src={li} alt="LinkedIn" />
+              </a>
+              <Link className="info" to="/info">
+                <img src={info} alt="More Info" />
+              </Link>
+            </div>
+          </div>
+          <div className="inner-header2">
+             <Link to="/">Home</Link>
+          </div>
+        </div>
+          <form method="post" action="https://app.thepoast.com/subscription/form">
+          <div className="input-wrapper">
+            <input className="email" type="text" name="firstName" placeholder="Your Name *" />
+          </div>
+          <div className="input-wrapper">
+            <input className="email" type="email" name="email" required placeholder="Email Address *" />
+            <button className="submit" type="submit">Subscribe</button>
+          </div>
+          <Altcha />
+          <input id="bcd05" type="hidden" name="l" checked value="bcd05274-b4b9-4a90-a8b5-1fdee92637fa" />
+          <input type="hidden" name="nonce" />
+        </form>
+          <img className="headerimg" src={j} alt="Instagram" />
+      </div>
+    </div>
+  );
+}
