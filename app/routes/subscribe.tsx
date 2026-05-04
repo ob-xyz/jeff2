a0c9453c0b1249a2aa80bf58074f626a10c62abd
wn <45404661+ob-xyz@users.noreply.github.com>
2026-05-04 15:42:53 -0400

    1

--- app/routes/subscribe.tsx
+++ app/routes/subscribe.tsx
@@ -37,7 +37,8 @@ export default function Index() {
         </div>
           <form method="post" action="https://app.thepoast.com/subscription/form">
           <div className="input-wrapper">
-            <input className="email" type="text" name="firstName" placeholder="Your Name *" />
+            <input className="email" type="text" name="firstName" placeholder="First Name *" />
+            <input className="email" type="text" name="lastName" placeholder="Last Name *" />
           </div>
           <div className="input-wrapper">
             <input className="email" type="email" name="email" required placeholder="Email Address *" />
